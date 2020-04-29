75f663ae8f3a16ee39aab00408682641679d2c1b
Richard Paquin-Morel <rap168@the-home-mac.local>
2020-04-29 10:36:30 -0500

    that good old initial commit

---
+++ R/rale.R
@@ -0,0 +1,24 @@
+#' Ride the rails
+#'
+#' Theme using Raleway as the main font
+#' Clean and minimal
+#'
+#' @inheritParams ggplot2::theme_minimal
+
+theme_rale <- function(base_size = 11, base_family = "Raleway") {
+
+ theme_minimal() +
+      theme(
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        text = element_text(family = "Raleway"),
+        axis.title.x = element_text(size = 9, hjust = 1),
+        axis.title.y = element_text(size = 9),
+        axis.text.x = element_text(family = "Lato", size = 8),
+        axis.text.y = element_text(family = "Lato", size = 8),
+        plot.title = element_text(size = 11, colour = "grey25", face = "bold"),
+        plot.subtitle = element_text(size = 10, colour = "grey45"),
+        plot.caption = element_text(size = 8, colour = "grey45")
+      )
+
+}
